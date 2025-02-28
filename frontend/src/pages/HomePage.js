bfcc7a73ec3e0c14afbf0dd517e0c3eef453ceed
mtthew-teng <196126034+mtthew-teng@users.noreply.github.com>
2025-02-28 00:50:16 -0800

    Created Home Page

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Job Tracker</h1>
+      <p>Track your job applications easily.</p>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+}
+
+export default HomePage;
